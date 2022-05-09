c67ae334ef6b8d21d69b041025295325e79fbdb7
Harold Leyton <87686471+Hxrxl@users.noreply.github.com>
2022-05-09 02:04:55 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/components/**/*.jsx',
+  ],
+ theme: {
+     extend: {},
+     backdropFilter: {
+      'none': 'none',
+      'blur': 'blur(20px)',
+    },
+ },
+ variants: {
+     extend: {},
+ },
+ plugins: [],
+};
